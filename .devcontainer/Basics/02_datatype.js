e663f4ce807c9e660f88091c56899b99b381a27c
AyushRai2007 <ayushraikhajo@gmail.com>
2026-05-24 12:36:17 +0000

    container config done

---
+++ .devcontainer/Basics/02_datatype.js
@@ -0,0 +1,14 @@
+"use strict";
+//alert(3+3)// we are using nodejs not browser
+//number
+//bigint
+//string
+//boolean
+//null =>standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null) //object
