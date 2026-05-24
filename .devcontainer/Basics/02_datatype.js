f2bba61d17bd0290f53399805ac819742bc99828
AyushRai2007 <ayushraikhajo@gmail.com>
2026-05-24 13:20:43 +0000

    container config done

--- .devcontainer/Basics/02_datatype.js
+++ .devcontainer/Basics/02_datatype.js
@@ -12,3 +12,13 @@
 
 console.log(typeof undefined)//undefined
 console.log(typeof null) //object
+
+//conversion
+ let score = "33"
+ console.log(typeof score)
+ console.log(typeof (score))
+
+ let ValueInNumber = Number(score)
+ console.log(typeof ValueInNumber)
+ console.log(ValueInNumber)
+//"33abc" "undefined" "string" =>NaN
