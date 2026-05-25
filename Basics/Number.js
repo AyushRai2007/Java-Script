2444d6b34b2ed4666b80d5753960b4174a7d575f
AyushRai2007 <ayushraikhajo@gmail.com>
2026-05-25 15:44:19 +0000

    Maths_Nums

---
+++ Basics/Number.js
@@ -0,0 +1,27 @@
+const balance = new Number(100)
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+//console.log(otherNumber.toPrecision(3));
+//priority left to right
+
+const Hund = 10000000
+//console.log(Hund.toLocaleString('en-In'));
+
+//++++++++++Maths++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(5.7));
+//ceil ,floor 
+console.log(Math.min(4,8,-2,7));
+//max
+console.log(Math.random());
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random()*(max-min+1))+ min)
+
+
