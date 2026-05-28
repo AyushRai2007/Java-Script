8156577ac39a6c26b038795fa06db0046af615d1
AyushRai2007 <ayushraikhajo@gmail.com>
2026-05-28 04:32:46 +0000

    Object

---
+++ NonPrimitive/Object_2.js
@@ -0,0 +1,18 @@
+//destructuring of object
+const course ={
+    coursename: "Js in Hindi",
+    coursePrice: "999",
+    courseInstructor: "Hitesh"
+}
+const {courseInstructor: instructor} = course
+//console.log(courseInstructor);
+console.log(instructor);
+
+//API
+{
+
+}
+[
+    {},
+    {}
+]
